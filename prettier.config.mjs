6e08d320da9981e8cde4c07c06af709e8000e018
Jose Gonzalez <jdgg2414@gmail.com>
2025-03-01 10:47:15 +0100

    format

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -5,6 +5,15 @@ const config = {
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
 
 export default config;
